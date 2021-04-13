0d6dbb0587bb04fa6cf89078bd056bca821673f6
Rohit BM <rohit991812@gmail.com>
2021-04-13 21:10:56 +0530

    added index, paste routes and faunaDB Integration

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-from flask import Flask, render_template, redirect
+import pytz
+from datetime import datetime
+from secrets import token_urlsafe
+from flask import Flask, render_template, redirect, request, abort
 from flask_bootstrap import Bootstrap
+from faunadb import query as q
+from faunadb.objects import Ref
+from faunadb.client import FaunaClient
+
 
 app = Flask(__name__)
+client = FaunaClient("fnAEGq8giCACBcibExwJxO_F5TCgGTeuW6_4Fkgh")
 Bootstrap(app)
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        title = request.form.get("title").strip()
+        paste_text = request.form.get("paste-text").strip()
+        identifier = token_urlsafe(5)
+        paste = client.query(q.create(q.collection("pastes"), {
+            "data": {
+                "identifier": identifier,
+                "paste_text": paste_text,
+                "title": title,
+                "date": datetime.now(pytz.UTC)
+            }
+        }))
+        return redirect(request.host_url + identifier)
     return render_template("index.html")
 
-
+@app.route("/<string:paste_id>/")
+def render_paste(paste_id):
+    try:
+        paste = client.query(
+            q.get(q.match(q.index("paste_by_identifier"), paste_id)))
+    except:
+        abort(404, description="Resource not found")
+    return render_template("paste.html", paste = paste["data"])
 
 if __name__=='__main__':
     app.run(debug=True)
